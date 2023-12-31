2546d0386129ec086f4035943b7f93e0f223ed0e
Yashmohi <mohiteyash77@gmail.com>
2023-12-31 23:44:38 +0530

    first commit

---
+++ src/app/my-mult.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'myMult',
+  standalone: true
+})
+export class MyMultPipe implements PipeTransform {
+
+   transform(value: number, Param: number): number
+  {
+    return value * Param;
+  }
+
+  }
+
+
